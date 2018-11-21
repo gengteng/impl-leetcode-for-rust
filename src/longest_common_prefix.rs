7e19d158ad504b673687878b9be0fe58fa514274
gengteng <me@gteng.org>
2018-11-21 21:06:48 +0800

    add solution to LongestCommonPrefix

--- src/longest_common_prefix.rs
+++ src/longest_common_prefix.rs
@@ -19,3 +19,37 @@
 pub trait LongestCommonPrefix {
     fn longest_common_prefix(strings: &[&str]) -> String;
 }
+
+pub struct Solution1;
+
+impl LongestCommonPrefix for Solution1 {
+    fn longest_common_prefix(strings: &[&str]) -> String {
+        let mut result = String::new();
+
+        if let Some(f) = strings.iter().nth(0) {
+            for (i, c) in f.chars().enumerate() {
+                for s in strings.iter().skip(1) {
+                    if let Some(v) = s.chars().nth(i) {
+                        if v != c {
+                            return result;
+                        }
+                    } else {
+                        return result;
+                    }
+                }
+
+                result.push(c);
+            }
+        }
+
+        result
+    }
+}
+
+#[test]
+fn test_solution1() {
+    assert_eq!(Solution1::longest_common_prefix(&["flower","flow","flight"]), "fl");
+    assert_eq!(Solution1::longest_common_prefix(&["dog","race_car","car"]), "");
+    assert_eq!(Solution1::longest_common_prefix(&["same","same","same"]), "same");
+    assert_eq!(Solution1::longest_common_prefix(&["test"]), "test");
+}
