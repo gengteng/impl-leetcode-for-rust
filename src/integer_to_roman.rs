920b329252bd8068a33fb1ac090f5434a19a7049
gengteng <me@gteng.org>
2018-11-27 20:57:21 +0800

    add solution to IntegerToRoman

--- src/integer_to_roman.rs
+++ src/integer_to_roman.rs
@@ -44,3 +44,49 @@
 pub trait IntegerToRoman {
     fn int_to_roman(num: u32) -> String;
 }
+
+pub struct Solution1;
+
+impl IntegerToRoman for Solution1 {
+    fn int_to_roman(num: u32) -> String {
+        let mut x = num;
+        let mut result = String::with_capacity(12);;
+
+        let table = [("M", 1000u32),
+            ("CM", 900), ("D", 500), ("CD", 400), ("C", 100),
+            ("XC", 90), ("L", 50), ("XL", 40), ("X", 10),
+            ("IX", 9), ("V", 5), ("IV", 4), ("I", 1)
+        ];
+
+        for (s, i) in table.iter() {
+            while x >= *i {
+                x -= i;
+                result.push_str(s);
+            }
+        }
+
+        result
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::IntegerToRoman;
+    use test::Bencher;
+
+    use super::Solution1;
+    #[test]
+    fn test_solution1() {
+        assert_eq!(Solution1::int_to_roman(3), "III");
+        assert_eq!(Solution1::int_to_roman(4), "IV");
+        assert_eq!(Solution1::int_to_roman(9), "IX");
+        assert_eq!(Solution1::int_to_roman(58), "LVIII");
+        assert_eq!(Solution1::int_to_roman(1994), "MCMXCIV");
+        assert_eq!(Solution1::int_to_roman(3333), "MMMCCCXXXIII");
+    }
+
+    #[bench]
+    fn bench_solution1(b: &mut Bencher) {
+        b.iter(|| Solution1::int_to_roman(1994));
+    }
+}
