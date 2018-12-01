ef0d2af86d512be8d12a3bf56b2ebb03577a016b
gengteng <me@gteng.org>
2018-12-02 00:14:02 +0800

    add solution to ThreeSumClosest

--- src/three_sum_closest.rs
+++ src/three_sum_closest.rs
@@ -10,3 +10,43 @@
 pub trait ThreeSumClosest {
     fn three_sum_closest(nums: &[i32], target: i32) -> i32;
 }
+
+pub struct Solution1;
+
+impl ThreeSumClosest for Solution1 {
+    fn three_sum_closest(nums: &[i32], target: i32) -> i32 {
+        let mut min_abs = std::i32::MAX;
+        let mut result = 0;
+
+        for (i, a) in nums.iter().enumerate() {
+            for (j, b) in nums.iter().skip(i + 1).enumerate() {
+                for c in nums.iter().skip(i + j + 2) {
+                    let abs = (a + b + c - target).abs();
+                    if abs < min_abs {
+                        result = a + b + c;
+                        min_abs = abs;
+                    }
+                }
+            }
+        }
+
+        result
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::ThreeSumClosest;
+    use test::Bencher;
+
+    use super::Solution1;
+    #[test]
+    fn test_solution1() {
+        assert_eq!(Solution1::three_sum_closest(&[-1, 2, -4, 1], 1), 2);
+    }
+
+    #[bench]
+    fn bench_solution1(b: &mut Bencher) {
+        b.iter(|| Solution1::three_sum_closest(&[-1, 2, 1, -4], 1));
+    }
+}
